eae1abdab582fc657176fac340d64682cd41ad2d
ligang <kingkongdog9527@gmail.com>
2026-01-17 23:26:41 +0800

    feat: 添加 maven 镜像源

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,6 +5,12 @@
 pluginManagement {
     includeBuild("build-logic")
     repositories {
+        maven { url=uri ("https://jitpack.io") }
+        maven { url=uri ("https://maven.aliyun.com/repository/releases") }
+        maven { url=uri ("https://maven.aliyun.com/repository/google") }
+        maven { url=uri ("https://maven.aliyun.com/repository/central") }
+        maven { url=uri ("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url=uri ("https://maven.aliyun.com/repository/public") }
         gradlePluginPortal()
         google()
         mavenCentral()
@@ -14,6 +20,12 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+        maven { url=uri ("https://jitpack.io") }
+        maven { url=uri ("https://maven.aliyun.com/repository/releases") }
+        maven { url=uri ("https://maven.aliyun.com/repository/google") }
+        maven { url=uri ("https://maven.aliyun.com/repository/central") }
+        maven { url=uri ("https://maven.aliyun.com/repository/gradle-plugin") }
+        maven { url=uri ("https://maven.aliyun.com/repository/public") }
         google()
         mavenCentral()
         maven("https://jitpack.io")
