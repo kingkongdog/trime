d031646b163117f3cccff03246386ef13f933a08
ligang <kingkongdog9527@gmail.com>
2026-02-08 20:46:14 +0800

    feat: test

--- app/src/main/java/com/osfans/trime/ime/candidates/CandidateItemUi.kt
+++ app/src/main/java/com/osfans/trime/ime/candidates/CandidateItemUi.kt
@@ -36,6 +36,7 @@ import splitties.views.dsl.core.view
 import splitties.views.dsl.core.wrapContent
 import splitties.views.gravityCenter
 import splitties.views.dip
+import splitties.views.updatePadding
 
 class CandidateItemUi(
     override val ctx: Context,
