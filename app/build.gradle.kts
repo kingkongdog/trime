91b3935f28c7e8fbee298d3d52f7f2aca3b3f07c
ligang <kingkongdog9527@gmail.com>
2026-07-26 21:44:17 +0800

    feat: versionCode to 20260902

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         applicationId = "com.osfans.trime"
         minSdk = 21
         targetSdk = 35
-        versionCode = 20260726
+        versionCode = 20260902
         versionName = "3.3.12"
 
         multiDexEnabled = true
